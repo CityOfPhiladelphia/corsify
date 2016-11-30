d7cfebb1433b2ce401579b64f39e76b9512c08ec
Robert Martin <rmartin@rmart.in>
2016-11-30 14:43:06 -0500

    Add comments to config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 CONFIG = {
+    # downstream hosts
     'origins': [
         'localhost',
         'cityofphiladelphia.github.io',
@@ -6,6 +7,7 @@ CONFIG = {
         'www.phila.gov',
         'atlas.phila.gov',
     ],
+    # upstream hosts
     'allowed_hosts': [
         'phila.gov',
         'www.phila.gov',
