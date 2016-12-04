7aee4a6288747ab1cb25bf25f607a6a2e9287d0e
Robert Martin <rmartin@rmart.in>
2016-12-03 19:54:47 -0500

    Add sample config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 CONFIG = {
     # downstream hosts
-    'origins': [
-        'localhost',
-        '0.0.0.0',
-    ],
-    # upstream hosts
-    'allowed_hosts': [
-        'phila.gov',
-        'www.phila.gov',
-    ],
+    # 'origins': [
+    #     'localhost',
+    #     '0.0.0.0',
+    # ],
+    # # upstream hosts
+    # 'allowed_hosts': [
+    #     'phila.gov',
+    #     'www.phila.gov',
+    # ],
 }
---
+++ config.sample.py
@@ -0,0 +1,13 @@
+CONFIG = {
+    # downstream hosts
+    'origins': [
+        'localhost',
+        'cityofphiladelphia.github.io',
+        'phila.gov',
+    ],
+    # # upstream hosts
+    'allowed_hosts': [
+        'phila.gov',
+        'www.phila.gov',
+    ],
+}
